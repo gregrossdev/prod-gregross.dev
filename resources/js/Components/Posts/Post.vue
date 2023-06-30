5fb53517b511cbab69cc24ca9751fdc67a3feead
Gregory Ross <gregross.dev@gmail.com>
2023-06-30 12:44:18 -0400

    check

--- resources/js/Components/Posts/Post.vue
+++ resources/js/Components/Posts/Post.vue
@@ -29,23 +29,23 @@ const editing = ref(false);
                     <span class="text-gray-800">{{ post.user.name }}</span>
                     <small v-if="post.created_at !== post.updated_at" class="text-sm text-gray-600"> &middot; edited</small>
                 </div>
-<!--                <Dropdown v-if="post.user.id === $page.props.auth.user.id">-->
-<!--                    <template #trigger>-->
-<!--                        <button>-->
-<!--                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">-->
-<!--                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />-->
-<!--                            </svg>-->
-<!--                        </button>-->
-<!--                    </template>-->
-<!--                    <template #content>-->
-<!--                        <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">-->
-<!--                            Edit-->
-<!--                        </button>-->
-<!--                        <DropdownLink as="button" :href="route('posts.destroy', post.id)" method="delete">-->
-<!--                            Delete-->
-<!--                        </DropdownLink>-->
-<!--                    </template>-->
-<!--                </Dropdown>-->
+                <Dropdown v-if="post.user.id === $page.props.auth.user.id">
+                    <template #trigger>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </template>
+                    <template #content>
+                        <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
+                            Edit
+                        </button>
+                        <DropdownLink as="button" :href="route('posts.destroy', post.id)" method="delete">
+                            Delete
+                        </DropdownLink>
+                    </template>
+                </Dropdown>
             </div>
             <form v-if="editing" @submit.prevent="form.put(route('posts.update', post.id), { onSuccess: () => editing = false })">
                 <textarea v-model="form.title" class="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
